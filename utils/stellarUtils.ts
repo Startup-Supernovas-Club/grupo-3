9063cf1942df58e3816ad2475b9a37d0548c8344
priscisolano06-ux <332721551+priscisolano06-ux@users.noreply.github.com>
2026-09-23 10:34:13 -0600

    Add Stellar address validation util

--- utils/stellarUtils.ts
+++ utils/stellarUtils.ts
@@ -1,3 +1,7 @@
 export function truncateAddress(address: string): string {
   return `${address.slice(0, 4)}...${address.slice(-4)}`
 }
+
+export function isValidStellarAddress(address: string): boolean {
+  return /^G[A-Z2-7]{55}$/.test(address)
+}
